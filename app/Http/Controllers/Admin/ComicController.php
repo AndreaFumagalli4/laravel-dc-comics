6a52752b8e3c8264b8c8126448291a028c9a4184
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2023-02-16 16:51:40 +0100

    add italian validation error display

--- app/Http/Controllers/Admin/ComicController.php
+++ app/Http/Controllers/Admin/ComicController.php
@@ -40,14 +40,36 @@ class ComicController extends Controller
         $data = $request->all();
 
         $request->validate([
-            'title' => 'required|min:2|max:100',
-            'description' => 'required|',
-            'thumb' => 'required|active_url',
-            'price' => 'required|decimal:2',
-            'series' => 'required|min:2|max:50',
-            'sale_date' => 'required|date|before:today',
-            'type' => 'required|min:2|max:30',
-            ]);
+                'title' => 'required|min:2|max:100',
+                'description' => 'required|min:10',
+                'thumb' => 'required|active_url',
+                'price' => 'required|decimal:2',
+                'series' => 'required|min:2|max:50',
+                'sale_date' => 'required|date|before:today|after:1934',
+                'type' => 'required|min:2|max:30',
+            ],
+            [
+                'title.required' => 'Il titolo è obbligatorio; per favore inserisci un titolo',
+                'title.min' => 'Hai scritto un titolo troppo corto; per favore inseriscine uno più lungo',
+                'title.max' => 'Hai scritto un titolo troppo lungo; per favore inseriscine uno più corto',
+                'description.required' => 'La descrizione è obbligatoria; per favore inseriscila',
+                'description.min' => 'Hai scritto una descrizione troppo corta; per favore inseriscine una più lunga',
+                'thumb.required' => 'L\'immagine è obbligatoria; per favore inseriscila',
+                'thumb.active_url' => 'L\'url che hai inserito non è valido: inseriscine uno valido',
+                'price.required' => 'Il prezzo è obbligatorio; per favore inseriscilo',
+                'price.decimal' => 'Hai scritto un prezzo sbagliato; inseriscine uno giusto',
+                'series.required' => 'La serie è obbligatoria; per favore inseriscila',
+                'series.min' => 'Hai scritto una serie troppo corta; per favore inseriscine uno più lunga',
+                'series.max' => 'Hai scritto una serei troppo lunga; per favore inseriscine uno più corta',
+                'sale_date.required' => 'La data di vendita è obbligatoria; per favore inseriscila',
+                'sale_date.date' => 'La data di vendita inserita è sbagliata: inseriscine una valida',
+                'sale_date.before' => 'La data di vendita inserita non è valida: inseriscine una valida',
+                'sale_date.after' => 'La data di vendita inserita non è valida: inseriscine una valida',
+                'type.required' => 'Il tipo di fumetto è obbligatorio; per favore inseriscilo',
+                'type.min' => 'Hai scritto una tipologia troppo corta; per favore inseriscine uno più lunga',
+                'type.max' => 'Hai scritto una tipologia troppo lunga; per favore inseriscine uno più corta'
+            ]
+        );
 
         $newComic = new Comic();
         $newComic->fill($data);
@@ -89,14 +111,36 @@ class ComicController extends Controller
         $data = $request->all();
 
         $request->validate([
-            'title' => 'required|min:2|max:100',
-            'description' => 'required|',
-            'thumb' => 'required|active_url',
-            'price' => 'required|decimal:2',
-            'series' => 'required|min:2|max:50',
-            'sale_date' => 'required|date|before:today',
-            'type' => 'required|min:2|max:30',
-            ]);
+                'title' => 'required|min:2|max:100',
+                'description' => 'required|min:10',
+                'thumb' => 'required|active_url',
+                'price' => 'required|decimal:2',
+                'series' => 'required|min:2|max:50',
+                'sale_date' => 'required|date|before:today',
+                'type' => 'required|min:2|max:30',
+            ],
+            [
+                'title.required' => 'Il titolo è obbligatorio; per favore inserisci un titolo',
+                'title.min' => 'Hai scritto un titolo troppo corto; per favore inseriscine uno più lungo',
+                'title.max' => 'Hai scritto un titolo troppo lungo; per favore inseriscine uno più corto',
+                'description.required' => 'La descrizione è obbligatoria; per favore inseriscila',
+                'description.min' => 'Hai scritto una descrizione troppo corta; per favore inseriscine una più lunga',
+                'thumb.required' => 'L\'immagine è obbligatoria; per favore inseriscila',
+                'thumb.active_url' => 'L\'url che hai inserito non è valido: inseriscine uno valido',
+                'price.required' => 'Il prezzo è obbligatorio; per favore inseriscilo',
+                'price.decimal' => 'Hai scritto un prezzo sbagliato; inseriscine uno giusto',
+                'series.required' => 'La serie è obbligatoria; per favore inseriscila',
+                'series.min' => 'Hai scritto una serie troppo corta; per favore inseriscine uno più lunga',
+                'series.max' => 'Hai scritto una serei troppo lunga; per favore inseriscine uno più corta',
+                'sale_date.required' => 'La data di vendita è obbligatoria; per favore inseriscila',
+                'sale_date.date' => 'La data di vendita inserita è sbagliata: inseriscine una valida',
+                'sale_date.before' => 'La data di vendita inserita non è valida: inseriscine una valida',
+                'sale_date.after' => 'La data di vendita inserita non è valida: inseriscine una valida',
+                'type.required' => 'Il tipo di fumetto è obbligatorio; per favore inseriscilo',
+                'type.min' => 'Hai scritto una tipologia troppo corta; per favore inseriscine uno più lunga',
+                'type.max' => 'Hai scritto una tipologia troppo lunga; per favore inseriscine uno più corta'
+            ]
+        );
 
         $comic = Comic::findOrFail($id);
         $comic->update($data);
